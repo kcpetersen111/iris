365dfefdd9b5b99b8ad431ad7433f1fb1fc646db
kaleb <kcpetersen111@gmail.com>
2023-02-05 18:11:22 -0700

    getting the http port to talk to the db

--- server/adapter/http/publicServer.go
+++ server/adapter/http/publicServer.go
@@ -1,7 +1,9 @@
 package http
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -9,22 +11,58 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func ping(_w http.ResponseWriter, _r *http.Request) {
-	fmt.Println("Hello, Iris")
-
+type Server interface {
+	Serve() (Server, error)
+}
+type IrisServer struct {
+	address string
+}
+type InputJson struct {
+	Message string `json:"message"`
 }
 
-func startServer() {
+func ping(w http.ResponseWriter, _r *http.Request) {
+	fmt.Println("Hello, Iris!")
+	message, err := json.Marshal("Hello, Iris!")
+	if err != nil {
+		log.Printf("Something went wrong when pinging the server\n")
+	}
+	w.Write(message)
+}
+
+func dbtest(w http.ResponseWriter, r *http.Request) {
+	var input InputJson
+	rawRequest, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading request: %v", err)
+	}
+	err = json.Unmarshal(rawRequest, &input)
+	if err != nil {
+		log.Printf("Error unmarshaling request: %v", err)
+	}
+
+	log.Printf("dbtest request body %+v,", input)
+
+	w.Write([]byte("message: " + input.Message))
+}
+
+func (s *IrisServer) Serve() {
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/ping", ping).Methods("GET")
+	router.HandleFunc("/test", dbtest).Methods("POST")
 
 	srv := &http.Server{
 		Handler:      router,
-		Addr:         "127.0.0.1:8000",
+		Addr:         s.address,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 	log.Fatal(srv.ListenAndServe())
+}
+func NewIrisServer(address string) *IrisServer {
+	return &IrisServer{
+		address: address,
+	}
 }
--- server/main.go
+++ server/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	httpServer "github.com/kcpetersen111/iris/server/adapter/http"
 	"github.com/kcpetersen111/iris/server/persist"
 )
 
 func main() {
-	_, err := persist.DbSetupConnection(true)
+	var setUpDB bool
+	flag.BoolVar(&setUpDB, "s", false, "Will run script to set up the database, Only needs to be done the first time starting up a server")
+	flag.Parse()
+
+	_, err := persist.DbSetupConnection(setUpDB)
 	if err != nil {
 		log.Fatalf("Error in starting up the database: %v", err)
 	}
-	http.startServer()
+	server := httpServer.NewIrisServer("localhost:8000")
+	server.Serve()
 }
